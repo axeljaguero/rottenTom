65d310612deb44bd91f83372fa89c106c75c7f84
nicolas nahuel <nicolasnahuel.pr@gmail.com>
2022-04-09 13:34:57 -0300

    feat: add view main

--- src/main/kotlin/windows/mainPage/RottenTomatoesMainWindow.kt
+++ src/main/kotlin/windows/mainPage/RottenTomatoesMainWindow.kt
@@ -1,55 +1,113 @@
 package windows.mainPage
 
-
-
-import models.category.CategoryModel
 import models.mainPage.RottenTomatoesViewModel
 import models.movie.MovieModel
+import models.category.CategoryModel
 import org.uqbar.arena.kotlin.extensions.*
+import org.uqbar.arena.widgets.Button
 import org.uqbar.arena.widgets.Panel
 import org.uqbar.arena.windows.SimpleWindow
 import org.uqbar.arena.windows.WindowOwner
 
 class RottenTomatoesMainWindow(owner: WindowOwner, model: RottenTomatoesViewModel) : SimpleWindow<RottenTomatoesViewModel>(owner, model) {
 
+    override fun addActions(actionsPanel: Panel?) {}
+    override fun createFormPanel(p0: Panel?) {}
 
-    override fun addActions(p0: Panel?) {
+    override fun createMainTemplate(mainPanel: Panel) {
+        title = "Cinepolis"
+        mainPanel.asVertical();
+        createListPanelCategory(mainPanel)
+        createListPanelMovie(mainPanel)
+
     }
+    private fun createListPanelMovie(mainPanel: Panel){
+        Panel(mainPanel) with {
+            asVertical()
+            table<MovieModel>(mainPanel) {
+                bindItemsTo("movieList")
+                bindSelectionTo("seletedMovie")
+                visibleRows = 10
+                column {
+                    title = "ID"
+                    fixedSize = 100
+                    bindContentsTo("id")
+                }
+                column {
+                    title = "Title"
+                    fixedSize = 200
+                    bindContentsTo("title")
+                }
+                column {
+                    title = "Poster"
+                    fixedSize = 200
+                    bindContentsTo("poster")
+                }
 
-    override fun createFormPanel(p0: Panel) {
-        table<CategoryModel>(p0) {
-            bindItemsTo("categorylist")
-            visibleRows = 10
-//            bindSelectionTo("selectedCategory")
-            column {
-                title = "ID"
-                fixedSize = 500
-                bindContentsTo("id")
             }
-            column {
-                title = "Name"
-                fixedSize = 100
-                bindContentsTo("name")
+
+        }
+
+        Panel(mainPanel) with {
+            asHorizontal()
+            Button(it) with {
+                //TODO
+                caption = "Add Category"
+                //bindEnabledTo("NADA")
+                onClick {
+
+                }
+            }
+            Button(it) with {
+                //TODO
+                caption = "Edit Category"
+                //bindEnabledTo("NADA")
+                onClick {
+
+                }
             }
         }
-        table<MovieModel>(p0) {
-            bindItemsTo("movieList")
-            visibleRows = 10
-            column {
-                title = "ID"
-                fixedSize = 500
-                bindContentsTo("id")
+    }
+
+    private fun createListPanelCategory(mainPanel: Panel){
+        Panel(mainPanel) with {
+            asVertical()
+            table<CategoryModel>(mainPanel) {
+                bindItemsTo("categorylist")
+                visibleRows = 10
+                bindSelectionTo("selectedCategory")
+                column {
+                    title = "ID"
+                    fixedSize = 100
+                    bindContentsTo("id")
+                }
+                column {
+                    title = "Title"
+                    fixedSize = 200
+                    bindContentsTo("name")
+                }
             }
-            column {
-                title = "Name"
-                fixedSize = 100
-                bindContentsTo("title")
+
+        }
+        Panel(mainPanel) with {
+            asHorizontal()
+            Button(it) with {
+                //TODO
+                caption = "Add Category"
+                //bindEnabledTo("NADA")
+                onClick {
+
+                }
             }
-            column {
-                title = "Poster"
-                fixedSize = 100
-                bindContentsTo("poster")
+            Button(it) with {
+                //TODO
+                caption = "Edit Category"
+                //bindEnabledTo("NADA")
+                onClick {
+
+                }
             }
         }
     }
 }
+
